c7888959505e722b6ea8584484713751199669c5
hmdnu <daniuyan71@gmail.com>
2025-05-04 18:38:57 +0700

    feat: basic routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,16 @@
 <?php
 
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\ProfessionCategoryController;
+use App\Http\Controllers\ProfessionController;
+use App\Http\Controllers\StudentController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\StudyProgramController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', fn() => view('welcome'));
+
+Route::resource('study-program', StudyProgramController::class);
+Route::resource('admin', AdminController::class);
+Route::resource('student', StudentController::class);
+Route::resource('profession', ProfessionController::class);
+Route::resource('profession-category', ProfessionCategoryController::class);
