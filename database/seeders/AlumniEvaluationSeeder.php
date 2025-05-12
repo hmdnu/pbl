85dfbd35735b9b731c611047e6bf8c7382ba8f5e
Gegas96 <gegasanugrah@gmail.com>
2025-05-12 17:07:43 +0700

    feat: AlumniEvaluation seeder

--- database/seeders/AlumniEvaluationSeeder.php
+++ database/seeders/AlumniEvaluationSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\AlumniEvaluation;
 
 class AlumniEvaluationSeeder extends Seeder
 {
@@ -12,6 +13,7 @@ class AlumniEvaluationSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+       AlumniEvaluation::factory()->count(5)->create();
+        
     }
 }
