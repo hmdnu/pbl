975248bf26409c1839feb989b8a7c17d129ba354
hmdnu <daniuyan71@gmail.com>
2025-05-16 09:21:01 +0700

    style: centered row table

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -13,6 +13,10 @@
             user-select: none;
         }
 
+        tr td {
+            vertical-align: middle
+        }
+
         @media (min-width: 768px) {
             .bd-placeholder-img-lg {
                 font-size: 3.5rem;
@@ -24,20 +28,7 @@
     @vite('resources/js/app.js')
 @endpush
 
-
-<header class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
-    <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3" href="#">Company name</a>
-    <button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-bs-toggle="collapse"
-        data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-    </button>
-    {{-- <input class="form-control form-control-dark w-100" type="text" placeholder="Search" aria-label="Search"> --}}
-    <div class="navbar-nav">
-        <div class="nav-item text-nowrap">
-            <a class="nav-link px-3" href="/logout">Sign out</a>
-        </div>
-    </div>
-</header>
+@include('partials.header')
 
 @section('content')
     @include('partials.sidebar')
