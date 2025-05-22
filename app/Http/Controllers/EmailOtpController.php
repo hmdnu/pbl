e5f9690db41c7d9ff66e11a4a223a9f905235c0b
hmdnu <daniuyan71@gmail.com>
2025-05-22 08:07:44 +0700

    feat: add otp alumni

--- app/Http/Controllers/EmailOtpController.php
+++ app/Http/Controllers/EmailOtpController.php
@@ -2,12 +2,84 @@
 
 namespace App\Http\Controllers;
 
+use App\Mail\SendOtp;
+use App\Models\Student;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\Mail;
 
 class EmailOtpController extends Controller
 {
-    public function sendOtp(Request $request)
+    public function sendOtpAlumni(Request $request)
     {
+        $validated = $request->validate([
+            'email' => ['required', 'email'],
+        ], [
+            'email.required' => 'Email tidak boleh kosong',
+            'email.email' => 'Email tidak valid',
+        ]);
 
+        $email = $validated['email'];
+        $mailSender = $this->sendOtp($email);
+        session()->put('otp_email', $email);
+
+        return back()
+            ->with([
+                'message' => $mailSender,
+            ])->withInput();
+    }
+
+    public function verifyOtpAlumni(Request $request)
+    {
+        $validated = $request->validate([
+            'otp' => ['required'],
+            'nim' => ['required']
+        ], [
+            'otp.required' => 'OTP tidak boleh kosong',
+            'nim.required' => 'NIM tidak boleh kosong'
+        ]);
+        $email = session('otp_email');
+        $cache = $this->verifyOtp($email);
+        $nim = Student::find($validated['nim']);
+
+        if ($cache === $validated['otp'] && $nim) {
+            return redirect()->route('view.alumni.form');
+        }
+
+        return back()->with([
+            'wrong_otp' => 'OTP salah'
+        ]);
+    }
+
+    public function sendOtpAlumniUser(Request $request)
+    {
+    }
+
+    public function verifyOtpAlumniUser(Request $request)
+    {
+    }
+
+    private function sendOtp($email)
+    {
+        $otp = rand(100000, 999999);
+        $ttlMinutes = 1;
+
+        $cache = Cache::put("otp:{$email}", $otp);
+        $mail = Mail::to($email)->send(new SendOtp($otp));
+
+        if (!$cache) {
+            return "Internal server error";
+        }
+
+        if (!$mail) {
+            return "Gagal mengirim email ke {$email}";
+        }
+
+        return "Kode OTP berhasil dikirim";
+    }
+
+    private function verifyOtp($email)
+    {
+        return Cache::get("otp:{$email}");
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\AlumniSurveyController;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\CrudTestController;
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\EmailOtpController;
 use App\Http\Controllers\ProfessionCategoryController;
 use App\Http\Controllers\ProfessionController;
 use App\Http\Controllers\StudentController;
@@ -12,6 +13,7 @@ use App\Http\Middleware\AdminAuth;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\StudyProgramController;
 use App\Http\Controllers\AlumniUserSurveyController;
+use Illuminate\Support\Facades\Redis;
 
 Route::get('/', fn() => view('welcome'));
 Route::get("/login", fn() => view('admin.login'));
@@ -40,9 +42,16 @@ Route::prefix('/survey')->group(function () {
     });
 
     Route::prefix('/alumni')->group(function () {
-        Route::get('/validation', [AlumniSurveyController::class, 'showValidation']);
-        Route::post('/validation', [AlumniSurveyController::class, 'submitValidation']);
-        Route::get('/form', [AlumniSurveyController::class, 'showform']);
-        Route::post('/form', [AlumniSurveyController::class, 'submitForm']);
+        Route::get('/validation', [AlumniSurveyController::class, 'showValidation'])->name('view.alumni.validation');
+        Route::post('/send-otp', [EmailOtpController::class, 'sendOtpAlumni'])->name('post.alumni.send-otp');
+        Route::post('/verifying-otp', [EmailOtpController::class, 'verifyOtpAlumni'])->name('post.alumni.verifying-otp');
+
+        Route::get('/form', [AlumniSurveyController::class, 'showform'])->name('view.alumni.form');
+        Route::post('/form', [AlumniSurveyController::class, 'submitForm'])->name('post.alumni.form');
     });
+});
+
+Route::get('/test-redis', function () {
+    Redis::set('hello', 'world');
+    return Redis::get('hello'); // should return "world"
 });
