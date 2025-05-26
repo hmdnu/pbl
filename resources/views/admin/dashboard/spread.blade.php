863fb9e7567027ee7059439b32ed87f2ae7db916
hmdnu <daniuyan71@gmail.com>
2025-05-26 20:37:06 +0700

    Fix inconsistent indentation in spread blade template
    Improved the readability of the spread.blade.php file by fixing inconsistent indentation in the section, table, and markup structure. These changes ensure better maintainability and cleaner code formatting.

--- resources/views/admin/dashboard/spread.blade.php
+++ resources/views/admin/dashboard/spread.blade.php
@@ -4,7 +4,8 @@
 
 
 @section('admin-content')
-    <section class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+    <section
+        class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
         <h1>Dashboard Sebaran</h1>
         <div class="btn-toolbar mb-2 mb-md-0">
             <div class="btn-group me-2">
@@ -36,19 +37,19 @@
         <div class="table-responsive">
             <table class="table table-striped table-sm">
                 <thead>
-                    <tr>
-                        <th scope="col">#</th>
-                        <th scope="col">Header</th>
-                        <th scope="col">Header</th>
-                        <th scope="col">Header</th>
-                        <th scope="col">Header</th>
-                    </tr>
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Header</th>
+                    <th scope="col">Header</th>
+                    <th scope="col">Header</th>
+                    <th scope="col">Header</th>
+                </tr>
                 </thead>
                 <tbody>
 
                 </tbody>
             </table>
+        </div>
     </section>
-    </div>
     @vite('resources/js/dashboard/spread.js')
 @endsection
