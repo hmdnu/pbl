4cc01fa43fbd258c3d8a442bac98ed800d6c8e4d
hmdnu <daniuyan71@gmail.com>
2025-06-05 13:51:27 +0700

    refactor: add proper iteration

--- resources/views/admin/program_study/table.blade.php
+++ resources/views/admin/program_study/table.blade.php
@@ -1,46 +1,48 @@
 <table class="table">
     <thead>
-        <tr>
-            <th scope="col">Id</th>
-            <th scope="col">Name</th>
-            <th scope="col">Action</th>
-        </tr>
+    <tr>
+        <th scope="col">No</th>
+        <th scope="col">Name</th>
+        <th scope="col">Action</th>
+    </tr>
     </thead>
     <tbody>
-        @foreach ($programstudies as $programstudy)
-            <tr>
-                <td>{{ $programstudy->id }}</td>
-                <td>{{ $programstudy->name }}</td>
-                <td>
-                    <button type="button" class="btn btn-primary" data-bs-toggle="modal"
+    @foreach ($programstudies as $programstudy)
+        <tr>
+            <td>{{ $loop->iteration }}</td>
+            <td>{{ $programstudy->name }}</td>
+            <td>
+                <button type="button" class="btn btn-primary" data-bs-toggle="modal"
                         data-bs-target="#modal-form-{{ $programstudy->id }}">
-                        Edit
-                    </button>
+                    Edit
+                </button>
 
-                    <button type="button" class="btn btn-danger" data-bs-toggle="modal"
+                <button type="button" class="btn btn-danger" data-bs-toggle="modal"
                         data-bs-target="#modal-confirm-{{ $programstudy->id }}">
-                        Delete
-                    </button>
+                    Delete
+                </button>
 
-                    <!-- Modal Edit -->
-                    <x-modal-form :id="$programstudy->id" title="Edit item" :action="url('/study-program/' . $programstudy->id)" method="PATCH">
+                <!-- Modal Edit -->
+                <x-modal-form :id="$programstudy->id" title="Edit item"
+                              :action="url('/study-program/' . $programstudy->id)" method="PATCH">
 
-                        @csrf
-                        @method('PATCH')
+                    @csrf
+                    @method('PATCH')
 
-                        <div class="input-group mb-3">
-                            <span class="input-group-text">Name</span>
-                            <input type="text" class="form-control" name="name" value="{{ $programstudy->name }}"
-                                required>
-                        </div>
-                    </x-modal-form>
+                    <div class="input-group mb-3">
+                        <span class="input-group-text">Name</span>
+                        <input type="text" class="form-control" name="name" value="{{ $programstudy->name }}"
+                               required>
+                    </div>
+                </x-modal-form>
 
-                    <!-- Modal Delete Confirmation -->
-                    <x-modal-confirm :id="$programstudy->id" title="Delete item {{ $programstudy->id }}" :action="url('/study-program/' . $programstudy->id)"
-                        method="DELETE" />
+                <!-- Modal Delete Confirmation -->
+                <x-modal-confirm :id="$programstudy->id" title="Delete item {{ $programstudy->id }}"
+                                 :action="url('/study-program/' . $programstudy->id)"
+                                 method="DELETE" />
 
-                </td>
-            </tr>
-        @endforeach
+            </td>
+        </tr>
+    @endforeach
     </tbody>
 </table>
--- resources/views/admin/table.blade.php
+++ resources/views/admin/table.blade.php
@@ -1,41 +1,47 @@
-
 <table class="table">
     <thead>
-        <tr>
-            <th scope="col">NIP</th>
-            <th scope="col">Nama</th>
-            <th scope="col">Action</th>
-        </tr>
+    <tr>
+        <th scope="col">No</th>
+        <th scope="col">NIP</th>
+        <th scope="col">Nama</th>
+        <th scope="col">Action</th>
+    </tr>
     </thead>
     <tbody>
-        @foreach ($admins as $admin)
-            <tr>
-                <td>{{ $admin->nip}}</td>
-                <td>{{ $admin->name}}</td>
-                <td>
-                    <div>
-                        <button type="button" class="btn btn-primary" data-bs-toggle="modal"
-                            data-bs-target="#modal-form-{{ $admin->nip }}">Edit</button>
-                        <button type="button" class="btn btn-danger" data-bs-target="#modal-confirm-{{ $admin->nip }}"
-                            data-bs-toggle="modal">Delete</button>
+    @foreach ($admins as $admin)
+        <tr>
+            <td>{{ $loop->iteration }}</td>
+            <td>{{ $admin->nip}}</td>
+            <td>{{ $admin->name}}</td>
+            <td>
+                <div>
+                    <button type="button" class="btn btn-primary" data-bs-toggle="modal"
+                            data-bs-target="#modal-form-{{ $admin->nip }}">Edit
+                    </button>
+                    <button type="button" class="btn btn-danger" data-bs-target="#modal-confirm-{{ $admin->nip }}"
+                            data-bs-toggle="modal">Delete
+                    </button>
 
-                        {{-- modal buat edit form --}}
-                        <x-modal-form :id="$admin->nip" title="Edit item" :action="url('/admin/' . $admin->nip)" method="PATCH">
-                            @csrf
-                            @method('PATCH')
-                            {{-- per item --}}
-                            <div class="input-group mb-3">
-                                <span class="input-group-text" id="basic-addon1">Nama</span>
-                                <input type="text" class="form-control" name="name" value="{{ $admin->name }}" placeholder="Nama" required>
-                            </div>
-                        </x-modal-form>
+                    {{-- modal buat edit form --}}
+                    <x-modal-form :id="$admin->nip" title="Edit item" :action="url('/admin/' . $admin->nip)"
+                                  method="PATCH">
+                        @csrf
+                        @method('PATCH')
+                        {{-- per item --}}
+                        <div class="input-group mb-3">
+                            <span class="input-group-text" id="basic-addon1">Nama</span>
+                            <input type="text" class="form-control" name="name" value="{{ $admin->name }}"
+                                   placeholder="Nama" required>
+                        </div>
+                    </x-modal-form>
 
-                        {{-- modal buat delete form --}}
-                        <x-modal-confirm :id="$admin->nip" title="Delete item {{ $admin->nip }}" :action="url('/admin/' . $admin->nip)"
-                        method="DELETE" />
-                    </div>
-                </td>
-            </tr>
-        @endforeach
+                    {{-- modal buat delete form --}}
+                    <x-modal-confirm :id="$admin->nip" title="Delete item {{ $admin->nip }}"
+                                     :action="url('/admin/' . $admin->nip)"
+                                     method="DELETE" />
+                </div>
+            </td>
+        </tr>
+    @endforeach
     </tbody>
 </table>
