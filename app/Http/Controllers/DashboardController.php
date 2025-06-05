0019a58ff877ad225ad495abf199a24d813c683e
hmdnu <daniuyan71@gmail.com>
2025-06-05 10:44:57 +0700

    refactor: reorder imports in DashboardController for consistency

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,9 +2,8 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
-use App\Models\Alumni;
+use Illuminate\Support\Facades\DB;
 
 class DashboardController extends Controller
 {
