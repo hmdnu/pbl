fdc45c633d5fbb261c3e9e1e20d8a05c94ac7df8
hmdnu <daniuyan71@gmail.com>
2025-06-10 10:12:39 +0700

    refactor: update sidebar styles and consolidate script imports in layouts
    - Adjusted padding and text color in `sidebar.css` for improved appearance.
    - Reorganized and deduplicated script imports in `app.blade.php` and `admin.blade.php` for consistency.

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -1,9 +1,6 @@
 @extends('layouts.app')
 
 @push('heads')
-    <!-- Bootstrap core CSS -->
-    <link rel="stylesheet" href="{{ asset('bootstrap/dist/css/bootstrap.min.css') }}">
-
     <style>
         .bd-placeholder-img {
             font-size: 1.125rem;
@@ -23,14 +20,10 @@
             }
         }
     </style>
-
-    @vite('resources/css/dashboard.css')
-    @vite('resources/js/app.js')
 @endpush
 
-@include('partials.header')
-
 @section('content')
+    @include('partials.header')
     <x-sidebar />
     <div class="container-fluid">
         <div class="row">
@@ -40,11 +33,4 @@
             </main>
         </div>
     </div>
-
-    <script src="https://cdn.jsdelivr.net/npm/feather-icons@4.28.0/dist/feather.min.js"
-            integrity="sha384-uO3SXW5IuS1ZpFPKugNNWqTZRRglnUJK6UAZ/gxOX80nxEkN9NcGZTftn6RzhGWE" crossorigin="anonymous">
-    </script>
-    <script src="https://cdn.jsdelivr.net/npm/chart.js@2.9.4/dist/Chart.min.js"
-            integrity="sha384-zNy6FEbO50N+Cg5wap8IKA4M/ZnLJgzc6w2NqACZaK0u0FXfOWRRJOnQtpZun8ha" crossorigin="anonymous">
-    </script>
 @endsection
